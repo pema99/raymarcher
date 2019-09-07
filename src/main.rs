f1ae3ef1209e6ae9f246f4228041e84ed8547966
pema99 <pemamalling@gmail.com>
2019-09-08 01:28:54 +0200

    Added CSG operators, box SDF, switched to minifb

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,88 @@
-extern crate image;
+#![allow(unused)]
+mod raymarcher;
+use raymarcher::Vec3;
+
 #[macro_use] extern crate impl_ops;
-use image::{ImageBuffer, RgbImage, Rgb};
-use std::f32::consts::PI;
-use std::ops;
-
-struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32
-}
-
-impl_op_ex!(+ |a: &Vec3, b: &Vec3| -> Vec3 { 
-    Vec3::new(a.x + b.x, a.y + b.y, a.z + b.z)
-});
-
-impl_op_ex!(- |a: &Vec3, b: &Vec3| -> Vec3 { 
-    Vec3::new(a.x - b.x, a.y - b.y, a.z - b.z)
-});
-
-impl_op_ex!(* |a: &Vec3, b: &f32| -> Vec3 { 
-    Vec3::new(a.x * b, a.y * b, a.z * b)
-});
+use std::f64::consts::PI;
+extern crate minifb;
+use minifb::{Key, WindowOptions, Window};
 
 impl Vec3 {
-    fn new(x: f32, y: f32, z: f32) -> Self {
-        Self {
-            x: x,
-            y: y,
-            z: z
-        }
-    }
+    fn to_color(&self) -> u32 {
+        let r = (self.x * 255.0) as u32;
+        let g = (self.y * 255.0) as u32;
+        let b = (self.z * 255.0) as u32;
 
-    fn magnitude(&self) -> f32 {
-        (self.x*self.x + self.y*self.y + self.z*self.z).sqrt()
-    }
-
-    fn normalize(&self) -> Self {
-        let mag = self.magnitude();
-        Self {
-            x: self.x / mag,
-            y: self.y / mag,
-            z: self.z / mag
-        }
-    }
-
-    fn to_rgb(&self) -> Rgb<u8> {
-        image::Rgb([
-            (self.x * 255.0) as u8, 
-            (self.y * 255.0) as u8, 
-            (self.z * 255.0) as u8])
+        b | (g << 8) | (r << 16) | (255 << 24)
     }
 }
 
 fn main() {
     let img_width = 600;
-    let img_height = 400; 
-    let fov = 75.0;
-    let aspect_ratio = img_width as f32 / img_height as f32;
-    let cam_orig = Vec3::new(0.0, 0.0, -10.0);
+    let img_height = 600; 
+    let fov = 90.0;
+    let mut cam_orig = Vec3::new(1.3, 1.0, -1.0);
 
-    let mut img: RgbImage = ImageBuffer::new(img_width, img_height);
-    
-    for x in 0..img_width {
-        for y in 0..img_height {
-            let far_x = (2.0 * ((x as f32 + 0.5) / img_width as f32) - 1.0) * (fov / 2.0 * PI / 180.0).tan() * aspect_ratio; 
-            let far_y = (1.0 - 2.0 * ((y as f32 + 0.5) / img_height as f32)) * (fov / 2.0 * PI / 180.0).tan(); 
-            
-            let ray_dir = (Vec3::new(far_x, far_y, 1.0) - &cam_orig).normalize();
+    let aspect_ratio = img_width as f64 / img_height as f64;
+    let inv_width = 1.0 / img_width as f64;
+    let inv_height = 1.0 / img_height as f64;
+    let view_angle = (PI * 0.5 * fov / 180.0).tan();
 
-            img.put_pixel(x, y, trace(&cam_orig, &ray_dir));
+    let mut buffer: Vec<u32> = vec![0; img_width * img_height];
+    let mut window = Window::new("Raymarcher thing", img_width, img_height, WindowOptions::default()).unwrap();
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        if window.is_key_down(Key::W) {
+            cam_orig.z += 0.1;
         }
+
+        for x in 0..img_width {
+            for y in 0..img_height {
+                //let far_x = (2.0 * ((x as f64 + 0.5) / img_width as f64) - 1.0) * (fov / 2.0 * PI / 180.0).tan() * aspect_ratio; 
+                //let far_y = (1.0 - 2.0 * ((y as f64 + 0.5) / img_height as f64)) * (fov / 2.0 * PI / 180.0).tan(); 
+                
+
+                let mut ray_dir = Vec3::new(
+                    (2.0 * (x as f64 * inv_width) - 1.0) * view_angle * aspect_ratio, 
+                    (1.0 - 2.0 * (y as f64 * inv_height)) * view_angle, 
+                    1.0).normalize();
+
+                //Rotate, temporary till i implement matrices
+                let y_rot: f64 = -PI / 4.0;
+                ray_dir = Vec3::new(
+                    ray_dir.x * y_rot.cos() + ray_dir.z * y_rot.sin(),
+                    ray_dir.y,
+                    -ray_dir.x * y_rot.sin() + ray_dir.z * y_rot.cos()
+                );
+                let x_rot: f64 = PI / 4.0;
+                ray_dir = Vec3::new(
+                    ray_dir.x,
+                    ray_dir.y * x_rot.cos() - ray_dir.z * x_rot.sin(),
+                    ray_dir.y * x_rot.sin() + ray_dir.z * x_rot.cos()
+                );
+
+                buffer[y*img_width+x] = trace(&cam_orig, &ray_dir);
+            }
+        }
+
+        window.update_with_buffer(&buffer).unwrap();
     }
 
-    img.save("epic.png");
+    //img.save("epic.png");
 }
 
-fn trace(orig: &Vec3, dir: &Vec3) -> Rgb<u8> {
+fn trace(orig: &Vec3, dir: &Vec3) -> u32 {
     let max_steps = 100;
     let max_dist = 100.0;
-    let epsilon = 0.000001;
+    let epsilon = 0.001;
 
     let mut depth = 0.0;
     let mut hit = false;
     
     for i in 0..max_steps {
-        let dist = sphere_sdf(orig + dir * depth);
+        let dist = scene_sdf(orig + dir * depth);
         
-        if dist < epsilon {
+        if dist.abs() < epsilon {        
             hit = true;
             break;
         }
@@ -99,13 +95,67 @@ fn trace(orig: &Vec3, dir: &Vec3) -> Rgb<u8> {
     }
 
     if hit {
-        Vec3::new((10.0-depth)*2.0, 0.0, 0.0).to_rgb()
+        //Vec3::new(1.0, 0.0, 0.0).to_color()
+        sdf_normal(orig + dir * depth).apply(&|v: f64| { v / 2.0 + 0.5 }).to_color()
     } 
     else {
-        Vec3::new(0.0, 0.0, 0.0).to_rgb()
+        Vec3::new(0.0, 0.0, 0.0).to_color()
     }  
 }
 
-fn sphere_sdf(from: Vec3) -> f32 {
-    (from - Vec3::new(0.0, 0.0, 0.0)).magnitude() - 0.4
+fn lerp(a: f64, b: f64, t: f64) -> f64 {
+    (1.0 - t) * a + t * b
+}
+
+fn intersect(a: f64, b: f64) -> f64 {
+    a.max(b)
+}
+
+fn union(a: f64, b: f64) -> f64 {
+    a.min(b)
+}
+
+fn difference(a: f64, b: f64) -> f64 {
+    a.max(-b)
+}
+
+fn union_smooth(a: f64, b: f64, k: f64) -> f64 {
+    let h = (0.5 + 0.5 * (a - b) / k).max(0.0).min(1.0);
+    lerp(a, b, h) - k * h * (1.0 - h)
+}
+
+fn intersect_smooth(a: f64, b: f64, k: f64) -> f64 {
+    union_smooth(a, b, -k)
+}
+
+fn difference_smooth(a: f64, b: f64, k: f64) -> f64 {
+    union_smooth(a, -b, -k)
+}
+
+fn sphere_sdf(from: &Vec3, center: Vec3, radius: f64) -> f64 {
+    //let center = Vec3::new(0.0, 0.0, 0.0);
+    (from - center).magnitude() - radius
+}
+
+fn box_sdf(p: &Vec3, b: &Vec3) -> f64 {
+    let d = p.abs() - b;
+    d.max(0.0).magnitude()
+    //(d.max(0.0)).magnitude() + d.x.max(d.y.max(d.z)).min(0.0)
+}
+
+fn scene_sdf(from: Vec3) -> f64 {
+    union_smooth(
+        //sphere_sdf(&from, Vec3::new(0.0, 0.3, 0.0), 0.5),
+        box_sdf(&(&from - Vec3::new(0.0, 0.0, 0.0)), &Vec3::new(0.5, 0.5, 0.5)),
+        sphere_sdf(&from, Vec3::new(0.0, 0.4, 0.0), 0.7),
+        0.1
+    )
+}
+
+fn sdf_normal(p: Vec3) -> Vec3 {
+    Vec3::new(
+        scene_sdf(Vec3::new(p.x + 0.00001, p.y, p.z)) - scene_sdf(Vec3::new(p.x - 0.00001, p.y, p.z)),
+        scene_sdf(Vec3::new(p.x, p.y + 0.00001, p.z)) - scene_sdf(Vec3::new(p.x, p.y - 0.00001, p.z)),
+        scene_sdf(Vec3::new(p.x, p.y, p.z  + 0.00001)) - scene_sdf(Vec3::new(p.x, p.y, p.z - 0.00001))
+    ).normalize()
 }
---
+++ src/raymarcher/vec3.rs
@@ -0,0 +1,71 @@
+use std::ops;
+
+#[derive(Debug)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+}
+
+impl_op_ex!(+ |a: &Vec3, b: &Vec3| -> Vec3 { 
+    Vec3::new(a.x + b.x, a.y + b.y, a.z + b.z)
+});
+
+impl_op_ex!(- |a: &Vec3, b: &Vec3| -> Vec3 { 
+    Vec3::new(a.x - b.x, a.y - b.y, a.z - b.z)
+});
+
+impl_op_ex!(* |a: &Vec3, b: &f64| -> Vec3 { 
+    Vec3::new(a.x * b, a.y * b, a.z * b)
+});
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self {
+            x: x,
+            y: y,
+            z: z
+        }
+    }
+
+    pub fn magnitude(&self) -> f64 {
+        (self.x*self.x + self.y*self.y + self.z*self.z).sqrt()
+    }
+
+    pub fn normalize(&self) -> Self {
+        let mag = self.magnitude();
+        Self {
+            x: self.x / mag,
+            y: self.y / mag,
+            z: self.z / mag
+        }
+    }
+
+    pub fn max(&self, val: f64) -> Self {
+        Self {
+            x: self.x.max(val),
+            y: self.y.max(val),
+            z: self.z.max(val)
+        }
+    }
+
+    pub fn min(&self, val: f64) -> Self {
+        Self {
+            x: self.x.min(val),
+            y: self.y.min(val),
+            z: self.z.min(val)
+        }
+    }
+
+    pub fn abs(&self) -> Vec3 {
+        self.apply(&f64::abs)
+    }
+
+    pub fn apply(&self, fun: &Fn(f64) -> f64) -> Self {
+        Self {
+            x: fun(self.x),
+            y: fun(self.y),
+            z: fun(self.z)
+        }
+    }
+}
