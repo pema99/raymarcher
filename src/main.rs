877a5c67052c97fc37e87e2e245d1145439d9a14
pema99 <pemamalling@gmail.com>
2019-09-09 23:28:16 +0200

    Refactor, start scene work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 mod raymarcher;
-use raymarcher::{Vec3, Mat4};
+use raymarcher::*;
 
 use std::f64::consts::PI;
 const EPSILON: f64 = 0.001;
@@ -19,6 +19,16 @@ const IMG_HEIGHT: usize = 600;
 const FOV: f64 = 90.0;
 
 fn main() {
+    let scene = Scene {
+        shapes: vec![
+            Box::new(
+                CSG::new(CSGOperator::UnionSmooth(0.1),
+                Box::new(Cube::new(Vec3::new(0.0, -0.5, 0.0), Vec3::new(0.5, 0.5, 0.5))),
+                Box::new(Sphere::new(Vec3::new(0.0, 0.4, 0.0), 0.7))
+            )),
+        ]
+    };
+
     //Mutable variables
     let mut cam_orig = Vec3::new(0.0, 0.0, -2.0);
     let mut cam_rot = Vec3::new(0.0, 0.0, 0.0);
@@ -77,7 +87,7 @@ fn main() {
 
                 ray_dir = rot_matrix * ray_dir;
 
-                trace(&cam_orig, &ray_dir)
+                trace(&scene, &cam_orig, &ray_dir)
             }).collect::<Vec<_>>()
         }).collect::<Vec<_>>();
 
@@ -93,18 +103,18 @@ fn main() {
 }
 
 //Trace a primary ray, return color
-fn trace(orig: &Vec3, dir: &Vec3) -> u32 {
+fn trace(scene: &Scene, orig: &Vec3, dir: &Vec3) -> u32 {
     let mut depth = 0.0;
 
     loop {
-        let dist = scene_sdf(orig + dir * depth);
+        let dist = scene.sdf(&(orig + dir * depth));
 
         if dist.abs() < EPSILON {
             //return sdf_normal(orig + dir * depth).apply(&|v: f64| v / 2.0 + 0.5).to_color();
 
             //Phong shading
             let hit = orig + dir * depth;
-            let normal = sdf_normal(hit);
+            let normal = scene.sdf_normal(hit);
 
             let light_source = Vec3::new(-1.0, 5.0, -2.0);          
 
@@ -126,59 +136,4 @@ fn trace(orig: &Vec3, dir: &Vec3) -> u32 {
     }
 
     Vec3::new(0.0, 0.0, 0.0).to_color()
-}
-
-fn lerp(a: f64, b: f64, t: f64) -> f64 {
-    (1.0 - t) * a + t * b
-}
-
-fn intersect(a: f64, b: f64) -> f64 {
-    a.max(b)
-}
-
-fn union(a: f64, b: f64) -> f64 {
-    a.min(b)
-}
-
-fn difference(a: f64, b: f64) -> f64 {
-    a.max(-b)
-}
-
-fn union_smooth(a: f64, b: f64, k: f64) -> f64 {
-    let h = (0.5 + 0.5 * (a - b) / k).max(0.0).min(1.0);
-    lerp(a, b, h) - k * h * (1.0 - h)
-}
-
-fn intersect_smooth(a: f64, b: f64, k: f64) -> f64 {
-    union_smooth(a, b, -k)
-}
-
-fn difference_smooth(a: f64, b: f64, k: f64) -> f64 {
-    union_smooth(a, -b, -k)
-}
-
-fn sphere_sdf(from: &Vec3, center: Vec3, radius: f64) -> f64 {
-    (from - center).magnitude() - radius
-}
-
-fn box_sdf(from: &Vec3, center: Vec3, size: Vec3) -> f64 {
-    let d = (from - center).abs() - size;
-    d.max(0.0).magnitude()
-    // + d.x.max(d.y.max(d.z)).min(0.0)
-}
-
-fn scene_sdf(from: Vec3) -> f64 {
-    union_smooth(
-        box_sdf(&from, Vec3::new(0.0, -0.5, 0.0), Vec3::new(0.5, 0.5, 0.5)),
-        sphere_sdf(&from, Vec3::new(0.0, 0.4, 0.0), 0.7),
-        0.1,
-    )
-}
-
-fn sdf_normal(p: Vec3) -> Vec3 {
-    Vec3::new(
-        scene_sdf(Vec3::new(p.x + EPSILON, p.y, p.z)) - scene_sdf(Vec3::new(p.x - EPSILON, p.y, p.z)),
-        scene_sdf(Vec3::new(p.x, p.y + EPSILON, p.z)) - scene_sdf(Vec3::new(p.x, p.y - EPSILON, p.z)),
-        scene_sdf(Vec3::new(p.x, p.y, p.z + EPSILON)) - scene_sdf(Vec3::new(p.x, p.y, p.z - EPSILON)),
-    ).normalize()
 }
--- src/raymarcher/scene.rs
+++ src/raymarcher/scene.rs
@@ -1,19 +1,16 @@
+use super::shapes::DistanceField;
 use super::Vec3;
-use super::math::*;
+use super::EPSILON;
 
-trait DistanceField{
-	fn SDF(&self, from: &Vec3) -> f64;
-}
-
-struct Scene {
-	shapes: Vec<Box<DistanceField>>
+pub struct Scene {
+	pub shapes: Vec<Box<DistanceField + Sync>>
 }
 
 impl DistanceField for Scene {
-	fn SDF(&self, from: &Vec3) -> f64 {
+    fn sdf(&self, from: &Vec3) -> f64 {
 		let mut min = std::f64::MAX;
 		for s in self.shapes.iter() {
-			let dist = s.SDF(from);
+			let dist = s.sdf(from);
 			if dist < min {
     			min = dist;
     		}
@@ -22,83 +19,13 @@ impl DistanceField for Scene {
 	}
 }
 
-struct Sphere {
-	center: Vec3,
-	radius: f64
+impl Scene {
+    pub fn sdf_normal(&self, p: Vec3) -> Vec3 {
+        Vec3::new(
+            self.sdf(&Vec3::new(p.x + EPSILON, p.y, p.z)) - self.sdf(&Vec3::new(p.x - EPSILON, p.y, p.z)),
+            self.sdf(&Vec3::new(p.x, p.y + EPSILON, p.z)) - self.sdf(&Vec3::new(p.x, p.y - EPSILON, p.z)),
+            self.sdf(&Vec3::new(p.x, p.y, p.z + EPSILON)) - self.sdf(&Vec3::new(p.x, p.y, p.z - EPSILON)),
+        ).normalize()
+    }
 }
 
-impl Sphere {
-	pub fn new(center: Vec3, radius: f64) -> Self {
-		Self {
-			center: center,
-			radius: radius
-		}
-	}
-}
-
-impl DistanceField for Sphere {
-	fn SDF(&self, from: &Vec3) -> f64 {
-		(from - self.center).magnitude() - self.radius
-	}
-}
-
-struct Cube {
-	center: Vec3,
-	size: Vec3
-}
-
-impl Cube {
-	pub fn new(center: Vec3, size: Vec3) -> Self {
-		Self {
-    		center: center,
-			size: size
-		}
-	}
-}
-
-impl DistanceField for Cube {
-	fn SDF(&self, from: &Vec3) -> f64 {
-		let diff = (from - self.center).abs() - self.size;
-		diff.max(0.0).magnitude()
-		// + diff.x.max(diff.y.max(diff.z)).min(0.0)
-	}
-}
-
-enum CSGOperator {
-	Union,
-	Intersect,
-	Difference,
-	UnionSmooth(f64),
-	IntersectSmooth(f64),
-	DifferenceSmooth(f64)
-}
-
-struct CSG {
-	a: Box<dyn DistanceField>,
-	b: Box<dyn DistanceField>,
-	op: CSGOperator
-}
-
-impl CSG {
-	pub fn new(op: CSGOperator, a: Box<dyn DistanceField>, b: Box<dyn DistanceField>) -> Self {
-		Self {
-			a: a,
-			b: b,
-			op: op
-		}
-	}
-}
-
-impl DistanceField for CSG {
-	fn SDF(&self, from: &Vec3) -> f64 {
-		match self.op {
-    		CSGOperator::Union => self.a.SDF(&from).min(self.b.SDF(&from)),
-    		CSGOperator::Intersect => self.a.SDF(&from).max(self.b.SDF(&from)),
-    		CSGOperator::Difference => self.a.SDF(&from).max(-self.b.SDF(&from)),
-    		CSGOperator::UnionSmooth(k) => min_smooth(self.a.SDF(&from), self.b.SDF(&from), k),
-			CSGOperator::IntersectSmooth(k) => max_smooth(self.a.SDF(&from), self.b.SDF(&from), k),
-			CSGOperator::DifferenceSmooth(k) => difference_smooth(self.a.SDF(&from), self.b.SDF(&from), k),
-    		_ => 0.0
-		}
-	}
-}
