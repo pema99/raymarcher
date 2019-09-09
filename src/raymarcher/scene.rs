e89b39c2388eb7615dc6da8c72f68e2fad137883
pema99 <pemamalling@gmail.com>
2019-09-09 23:42:58 +0200

    Tabs

--- src/raymarcher/scene.rs
+++ src/raymarcher/scene.rs
@@ -7,25 +7,25 @@ pub struct Scene {
 }
 
 impl DistanceField for Scene {
-    fn sdf(&self, from: &Vec3) -> f64 {
+	fn sdf(&self, from: &Vec3) -> f64 {
 		let mut min = std::f64::MAX;
 		for s in self.shapes.iter() {
 			let dist = s.sdf(from);
 			if dist < min {
-    			min = dist;
-    		}
+				min = dist;
+			}
 		}
 		min
 	}
 }
 
 impl Scene {
-    pub fn sdf_normal(&self, p: Vec3) -> Vec3 {
-        Vec3::new(
-            self.sdf(&Vec3::new(p.x + EPSILON, p.y, p.z)) - self.sdf(&Vec3::new(p.x - EPSILON, p.y, p.z)),
-            self.sdf(&Vec3::new(p.x, p.y + EPSILON, p.z)) - self.sdf(&Vec3::new(p.x, p.y - EPSILON, p.z)),
-            self.sdf(&Vec3::new(p.x, p.y, p.z + EPSILON)) - self.sdf(&Vec3::new(p.x, p.y, p.z - EPSILON)),
-        ).normalize()
-    }
+	pub fn sdf_normal(&self, p: Vec3) -> Vec3 {
+		Vec3::new(
+			self.sdf(&Vec3::new(p.x + EPSILON, p.y, p.z)) - self.sdf(&Vec3::new(p.x - EPSILON, p.y, p.z)),
+			self.sdf(&Vec3::new(p.x, p.y + EPSILON, p.z)) - self.sdf(&Vec3::new(p.x, p.y - EPSILON, p.z)),
+			self.sdf(&Vec3::new(p.x, p.y, p.z + EPSILON)) - self.sdf(&Vec3::new(p.x, p.y, p.z - EPSILON)),
+		).normalize()
+	}
 }
 
